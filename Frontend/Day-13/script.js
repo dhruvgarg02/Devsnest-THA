e31e4a5559660fd0316e0deabacd291b94f87139
Dhruv Garg <dhruvgargg02@gmail.com>
2021-07-07 16:54:16 +0530

    Completed Day-13

---
+++ Frontend/Day-13/script.js
@@ -0,0 +1,42 @@
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var feelsLike = document.querySelector('.feelsLike');
+var clouds = document.querySelector('.clouds');
+var humidity = document.querySelector('.humidity');
+var button = document.querySelector('.submit');
+
+document.addEventListener('keydown', (key) => {
+    if (key.keyCode === 13) {
+        fetchWeather()
+    } else {
+        return;
+    }
+})
+
+button.addEventListener('click', fetchWeather)
+
+function fetchWeather() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=ac5b4d29fe3ee03476d1ed5132a58d7a' + '&units=metric')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            var tempValue = data['main']['temp'];
+            var feels_Like = data['main']['feels_like']
+            var nameValue = data['name'];
+            var country = data['sys']['country'];
+            var descValue = data['weather'][0]['main'];
+            var humidityValue = data['main']['humidity'];
+
+            main.innerHTML = nameValue + "," + country;
+            desc.innerHTML = descValue;
+            temp.innerHTML = "Temperature - " + tempValue + "°C";
+            feelsLike.innerHTML = "Feels Like - " + feels_Like + "°C";
+            humidity.innerHTML = "Humidity - " + humidityValue;
+            input.value = "";
+
+        })
+
+    .catch(err => alert("Wrong city name!"));
+}
