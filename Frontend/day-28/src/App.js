4ec5b377c8d454b09c7d61d62aed6097491d21ec
Dhruv Garg <dhruvgargg02@gmail.com>
2021-08-04 10:51:12 +0530

    completed day-28

--- Frontend/day-28/src/App.js
+++ Frontend/day-28/src/App.js
@@ -4,10 +4,11 @@ import Card from "./component/Card";
 import {useState} from "react";
 
 function App() {
-     const [data, setData] = useState({})
+    const [data, setData] = useState({})
+    const API_KEY = process.env.REACT_APP_FINANCE_API_KEY;
     const getApiData = (fromCurrency, toCurrency) => {
         fetch(
-            `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${fromCurrency}&to_currency=${toCurrency}&apikey=4MISL5M4OM7ANAOZ`
+            `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${fromCurrency}&to_currency=${toCurrency}&apikey=${API_KEY}`
         ).then(response => response.json())
             .then(data => {
                 setData(data)
--- Frontend/day-28/src/component/CurrencySelector.js
+++ Frontend/day-28/src/component/CurrencySelector.js
@@ -11,24 +11,49 @@ const CurrencySelector = ({getApiData}) => {
     }
 
     return <form onSubmit={HandleInput}>
-            <select id="from-currency" className="dropdown-content" onChange={e => {
-                setFromCurrency(e.target.value)
-            }}
-            >
-                <option value="">Select From</option>
-                <option value="BTC">Bitcoin</option>
-                <option value="USD">US Dollar</option>
-                <option value="INR">Indian Rupee</option>
-            </select>
+        <select id="from-currency" className="dropdown-content" onChange={e => {
+            setFromCurrency(e.target.value)
+        }}
+        >
+            <option value="">Select From</option>
+            <option value="BTC">Bitcoin</option>
+            <option value="DICE">Etheroll</option>
+            <option value="XRP">Ripple</option>
+            <option value="ADA">Cardano</option>
+            <option value="XLM">Stellar</option>
+            <option value="LINK">Chainlink</option>
+            <option value="UNI">Uniswap</option>
+            <option value="DOT">Polkadot</option>
+            <option value="DOGE">DogeCoin</option>
+            <option value="DOPE">DopeCoin</option>
+            <option value="USD">US Dollar</option>
+            <option value="INR">Indian Rupee</option>
+            <option value="EUR">Euro</option>
+            <option value="JPY">Japanese Yen</option>
+            <option value="GBP">British Pound Sterling</option>
+            <option value="AUD">Australian Dollar</option>
+            <option value="CAD">Canadian Dollar</option>
+            <option value="CHF">Swiss Franc</option>
+            <option value="CAD">Canadian Dollar</option>
+            <option value="NZD">New Zealand Dollar</option>
+        </select>
 
-            <select id="to-currency" className="dropdown-content" onChange={e => {
-                setToCurrency(e.target.value)
-            }}>
-                <option value="">Select To</option>
-                <option value="BTC">Bitcoin</option>
-                <option value="USD">US Dollar</option>
-                <option value="INR">Indian Rupee</option>
-            </select>
+        <select id="to-currency" className="dropdown-content" onChange={e => {
+            setToCurrency(e.target.value)
+        }}>
+            <option value="">Select To</option>
+            <option value="USD">US Dollar</option>
+            <option value="INR">Indian Rupee</option>
+            <option value="EUR">Euro</option>
+            <option value="JPY">Japanese Yen</option>
+            <option value="GBP">British Pound Sterling</option>
+            <option value="AUD">Australian Dollar</option>
+            <option value="CAD">Canadian Dollar</option>
+            <option value="CHF">Swiss Franc</option>
+            <option value="CAD">Canadian Dollar</option>
+            <option value="NZD">New Zealand Dollar</option>
+
+        </select>
 
         <input type="submit" value="Submit"/>
     </form>
