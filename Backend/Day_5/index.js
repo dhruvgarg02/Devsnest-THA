4565e5db3bed8795db5ec0367567f7424450d833
Dhruv Garg <dhruvgargg02@gmail.com>
2021-09-03 20:17:58 +0530

    Day 5

--- Backend/Day_5/index.js
+++ Backend/Day_5/index.js
@@ -1,3 +1,4 @@
-var fs = require("fs");
-fs.mkdirSync("day_5");
-fs.writeFileSync("day_5/hello.txt", "Hello, NodeJS !!");
+var Test = require("./require_test");
+
+var obj = new Test();
+console.log(obj.print());
---
+++ Backend/Day_5/require_test.js
@@ -0,0 +1,13 @@
+class Test{
+    print() {
+        console.log("I'm inside require_test");
+    }
+}
+
+class Test2 {
+    print() {
+        console.log(">>>>>>>>>>>");
+    }
+}
+
+module.exports = Test;
